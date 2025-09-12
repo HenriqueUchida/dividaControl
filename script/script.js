5d966dc2b7824fd53c21f8175e1176a39b64c4bf
HenriqueUchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-09-12 02:17:02 +0000

    implementando o envio das receitas

--- script/script.js
+++ script/script.js
@@ -16,9 +16,10 @@ let qtdeParcelas = document.getElementById('qtde-parcelas')
 let parcelaAtual = 1
 let radioAvista = document.getElementById('a-vista')
 let radioParcelado = document.querySelector('#parcelado')
+let containerCategoria = document.querySelector('#container-categoria')
 let categoria = document.querySelector('#categoria')
 let dataInicio = new Date()
-let dia = String(dataInicio.getDate()).padStart(2, '0')
+let dia = String(dataInicio.getDate()).padStart(2, '0')//padStart é tipo o LPAD do oracle, nesse caso, o comando diz que vai ter dois caracteres e completar com 0.
 let mes = String(dataInicio.getMonth() + 1).padStart(2, '0') //Em js os meses são de 0 a 11, por isso soma-se 1
 let ano = String(dataInicio.getFullYear())
 let dataFormatada = `${dia}/${mes}/${ano}`
@@ -123,119 +124,153 @@ motivo.addEventListener('keydown', function(event) {
 
 async function envia(event) {
     event.preventDefault()
-    if (val.value == '' || motivo.value == '' || qtdeParcelas.value == '') {
-        event.preventDefault()
-        alert('Preencha todos os campos antes de enviar')
-        txt.innerHTML = ''
-        if (val.value == ''){
-            val.focus()
-        } else if (motivo.value == '') {
-            motivo.focus()
-        } else if (qtdeParcelas.value == '') {
+    if (controlPage === 'despesa') {
+        if (val.value == '' || motivo.value == '' || qtdeParcelas.value == '') {
+            event.preventDefault()
+            alert('Preencha todos os campos antes de enviar')
+            txt.innerHTML = ''
+            if (val.value == ''){
+                val.focus()
+            } else if (motivo.value == '') {
+                motivo.focus()
+            } else if (qtdeParcelas.value == '') {
+                qtdeParcelas.focus()
+            }
+            return
+        } else if (radioAvista.checked != true && qtdeParcelas.value == 1) {
+            alert('[ERRO]Numero de parcela inválido!')
+            qtdeParcelas.value = 2
             qtdeParcelas.focus()
-        }
-        return
-    } else if (radioAvista.checked != true && qtdeParcelas.value == 1) {
-        alert('[ERRO]Numero de parcela inválido!')
-        qtdeParcelas.value = 2
-        qtdeParcelas.focus()
-        return
-     } if (radioAvista.checked != true && qtdeParcelas.value > 1){
-            txt.style.display = 'block'
-            valAPagar = (Number(val.value)/Number(qtdeParcelas.value)).toFixed(2)
-            let values = []
-            let dados = {}
-            for(let i = 1; i<=qtdeParcelas.value; i++){
-                let motivoArr = ''
-                let dataVencimento = new Date(dataInicio.getFullYear(), dataInicio.getMonth(), dataInicio.getDate())
-                dataVencimento.setMonth(dataVencimento.getMonth() + (i))
-                const dia = String(dataVencimento.getDate()).padStart(2, '0')
-                const mes = String(dataVencimento.getMonth() + 1).padStart(2, '0')
-                const ano = dataVencimento.getFullYear()
-                dataFormatada = `${dia}/${mes}/${ano}`
-                // console.log(motivoArr, 'antes de incluir')
-                motivoArr = `${motivo.value} ${i}/${qtdeParcelas.value}`
-                let linhasArr = []
+            return
+        } if (radioAvista.checked != true && qtdeParcelas.value > 1){
+                txt.style.display = 'block'
+                valAPagar = (Number(val.value)/Number(qtdeParcelas.value)).toFixed(2)
+                let values = []
+                let dados = {}
+                for(let i = 1; i<=qtdeParcelas.value; i++){
+                    let motivoArr = ''
+                    let dataVencimento = new Date(dataInicio.getFullYear(), dataInicio.getMonth(), dataInicio.getDate())
+                    dataVencimento.setMonth(dataVencimento.getMonth() + (i))
+                    const dia = String(dataVencimento.getDate()).padStart(2, '0')
+                    const mes = String(dataVencimento.getMonth() + 1).padStart(2, '0')
+                    const ano = dataVencimento.getFullYear()
+                    dataFormatada = `${dia}/${mes}/${ano}`
+                    // console.log(motivoArr, 'antes de incluir')
+                    motivoArr = `${motivo.value} ${i}/${qtdeParcelas.value}`
+                    let linhasArr = []
+                    linhasArr.push(dataFormatada)
+                    linhasArr.push(controlPage.toUpperCase())
+                    linhasArr.push(cartoes.value.toUpperCase())
+                    linhasArr.push(pagto.value.toUpperCase())
+                    linhasArr.push(modalidade.toUpperCase())
+                    linhasArr.push(qtdeParcelas.value)
+                    linhasArr.push(valAPagar) // não precisa do .value porque não veio do HTML
+                    linhasArr.push(val.value)
+                    linhasArr.push(motivoArr.toUpperCase())
+                    linhasArr.push(categoria.value.toUpperCase())
+                    // console.log(linhasArr, 'depois de incluir')
+                    values.push(linhasArr)
+                    
+                }
+                dados.data = dataFormatada
+                dados.tipo = controlPage.toUpperCase()
+                dados.pagamento = pagto.value.toUpperCase()
+                dados.cartao = cartao.value.toUpperCase()
+                dados.modalidade = modalidade.toUpperCase()
+                dados.parcelas = qtdeParcelas.value
+                dados.valor = val.value
+                dados.motivo = motivo.value.toUpperCase()
+                dados.categoria = categoria.value.toUpperCase()
+                txt.innerHTML = `
+                    <h3>Último Envio:</h3>
+                    <p><strong>Data:</strong> ${dados.data}</p>
+                    <p><strong>Tipo:</strong> ${dados.tipo}</p>
+                    <p><strong>Pagamento:</strong> ${dados.pagamento}</p>
+                    <p><strong>Cartao:</strong> ${dados.cartao}</p>
+                    <p><strong>Modalidade:</strong> ${dados.modalidade}</p>
+                    <p><strong>Parcelas:</strong> ${dados.parcelas}</p>
+                    <p><strong>Valor:</strong> ${dados.valor}</p>
+                    <p><strong>Motivo:</strong> ${dados.motivo}</p>
+                    <p><strong>Categoria:</strong> ${dados.categoria}</p>
+                `
+                alert(`até aqui deu certo - ${dataFormatada}`)
+                console.log(values)
+                debugger
+                enviar.submit()
+
+            } else {
+                linhasArr = []
+                values = []
+                txt.style.display = 'block'
+                dados.data = dataFormatada
+                dados.tipo = controlPage.toUpperCase()
+                dados.pagamento = pagto.value.toUpperCase()
+                dados.cartao = cartao.value.toUpperCase()
+                dados.modalidade = modalidade.toUpperCase()
+                dados.parcelas = qtdeParcelas.value
+                dados.valor = val.value
+                dados.motivo = motivo.value.toUpperCase()
+                dados.categoria = categoria.value.toUpperCase()
                 linhasArr.push(dataFormatada)
-                linhasArr.push(controlPage.toUpperCase())
-                linhasArr.push(cartoes.value.toUpperCase())
+                linhasArr.push(controlPage.toUpperCase()) 
                 linhasArr.push(pagto.value.toUpperCase())
+                linhasArr.push(cartoes.value.toUpperCase())
                 linhasArr.push(modalidade.toUpperCase())
                 linhasArr.push(qtdeParcelas.value)
-                linhasArr.push(valAPagar) // não precisa do .value porque não veio do HTML
+                linhasArr.push(valAPagar.value) 
                 linhasArr.push(val.value)
-                linhasArr.push(motivoArr.toUpperCase())
+                linhasArr.push(motivo.value.toUpperCase())
                 linhasArr.push(categoria.value.toUpperCase())
-                // console.log(linhasArr, 'depois de incluir')
                 values.push(linhasArr)
-                
+                txt.innerHTML = `
+                    <h3>Último Envio:</h3>
+                    <p><strong>Data:</strong> ${dados.data}</p>
+                    <p><strong>Tipo:</strong> ${dados.tipo}</p>
+                    <p><strong>Pagamento:</strong> ${dados.pagamento}</p>
+                    <p><strong>Cartao:</strong> ${dados.cartao}</p>
+                    <p><strong>Modalidade:</strong> ${dados.modalidade}</p>
+                    <p><strong>Parcelas:</strong> ${dados.parcelas}</p>
+                    <p><strong>Valor:</strong> ${dados.valor}</p>
+                    <p><strong>Motivo:</strong> ${dados.motivo}</p>
+                    <p><strong>Categoria:</strong> ${dados.categoria}</p>
+                `
+                alert(`até aqui deu certo - ${dataFormatada}`)
+                console.log(values)
+                debugger
+                enviar.submit()
             }
-            dados.data = dataFormatada
-            dados.tipo = controlPage.toUpperCase()
-            dados.pagamento = pagto.value.toUpperCase()
-            dados.cartao = cartao.value.toUpperCase()
-            dados.modalidade = modalidade.toUpperCase()
-            dados.parcelas = qtdeParcelas.value
-            dados.valor = val.value
-            dados.motivo = motivo.value.toUpperCase()
-            dados.categoria = categoria.value.toUpperCase()
-            txt.innerHTML = `
-                <h3>Último Envio:</h3>
-                <p><strong>Data:</strong> ${dados.data}</p>
-                <p><strong>Tipo:</strong> ${dados.tipo}</p>
-                <p><strong>Pagamento:</strong> ${dados.pagamento}</p>
-                <p><strong>Cartao:</strong> ${dados.cartao}</p>
-                <p><strong>Modalidade:</strong> ${dados.modalidade}</p>
-                <p><strong>Parcelas:</strong> ${dados.parcelas}</p>
-                <p><strong>Valor:</strong> ${dados.valor}</p>
-                <p><strong>Motivo:</strong> ${dados.motivo}</p>
-                <p><strong>Categoria:</strong> ${dados.categoria}</p>
-            `
-            alert(`até aqui deu certo - ${dataFormatada}`)
-            console.log(values)
-            debugger
-            enviar.submit()
-
         } else {
-            linhasArr = []
-            values = []
-            txt.style.display = 'block'
-            dados.data = dataFormatada
-            dados.tipo = controlPage.toUpperCase()
-            dados.pagamento = pagto.value.toUpperCase()
-            dados.cartao = cartao.value.toUpperCase()
-            dados.modalidade = modalidade.toUpperCase()
-            dados.parcelas = qtdeParcelas.value
-            dados.valor = val.value
-            dados.motivo = motivo.value.toUpperCase()
-            dados.categoria = categoria.value.toUpperCase()
-            linhasArr.push(dataFormatada)
-            linhasArr.push(controlPage.toUpperCase()) 
-            linhasArr.push(pagto.value.toUpperCase())
-            linhasArr.push(cartoes.value.toUpperCase())
-            linhasArr.push(modalidade.toUpperCase())
-            linhasArr.push(qtdeParcelas.value)
-            linhasArr.push(valAPagar.value) 
-            linhasArr.push(val.value)
-            linhasArr.push(motivo.value.toUpperCase())
-            linhasArr.push(categoria.value.toUpperCase())
-            values.push(linhasArr)
-            txt.innerHTML = `
-                <h3>Último Envio:</h3>
-                <p><strong>Data:</strong> ${dados.data}</p>
-                <p><strong>Tipo:</strong> ${dados.tipo}</p>
-                <p><strong>Pagamento:</strong> ${dados.pagamento}</p>
-                <p><strong>Cartao:</strong> ${dados.cartao}</p>
-                <p><strong>Modalidade:</strong> ${dados.modalidade}</p>
-                <p><strong>Parcelas:</strong> ${dados.parcelas}</p>
-                <p><strong>Valor:</strong> ${dados.valor}</p>
-                <p><strong>Motivo:</strong> ${dados.motivo}</p>
-                <p><strong>Categoria:</strong> ${dados.categoria}</p>
-            `
-            alert(`até aqui deu certo - ${dataFormatada}`)
-            console.log(values)
-            debugger
-            enviar.submit()
+            let values = []
+            let linhasArr = []
+            let categoria = 'salario'
+            pagto.value = 'pix'
+            qtdeParcelas.value = 1
+            cartoes.value = ''
+            modalidade = 'A vista'
+            if (val.value == '' || motivo.value == '') {
+                event.preventDefault()
+                alert('Informe todos os valores antes de enviar')
+            } else if (val.value == '') {
+                event.preventDefault()
+                val.focus()
+            } else if (motivo.value == '') {
+                event.preventDefault()
+                motivo.focus()
+            return
+            } else {
+                linhasArr.push(dataFormatada)
+                linhasArr.push(controlPage.toUpperCase()) 
+                linhasArr.push(pagto.value.toUpperCase())
+                linhasArr.push(cartoes.value.toUpperCase())
+                linhasArr.push(modalidade.toUpperCase())
+                linhasArr.push(qtdeParcelas.value)
+                linhasArr.push(valAPagar.value) 
+                linhasArr.push(val.value)
+                linhasArr.push(motivo.value.toUpperCase())
+                linhasArr.push(categoria.toUpperCase())
+                values.push(linhasArr)
+            }
+        console.log(values)    
         }
     const apiUrl = 'https://dividacontrol.onrender.com/addRow';        
     try {
@@ -288,6 +323,7 @@ function trocaPagina() {
         btnReceita.style.color = '#ffffff'
         btnDespesa.style.textDecoration = 'underline'
         btnReceita.style.textDecoration = 'none'
+        containerCategoria.style.display = 'block'
         inicoPadrao()
     } else if (controlPage === 'receita') {
         pagto.value = 'pix'
@@ -304,6 +340,7 @@ function trocaPagina() {
         btnDespesa.style.color = '#ffffff'
         btnReceita.style.textDecoration = 'underline'
         btnDespesa.style.textDecoration = 'none'
+        containerCategoria.style.display = 'none'
     }
 }
 
